46e26849e6b2b7518aa1c0ab1dfe19b219056d20
KAH <kahback99@gmail.com>
2025-01-27 17:39:00 +0900

    [feat] 시큐리티 설정 추가

---
+++ backend/clean-meet/src/main/java/com/project/spring/cleanmeet/common/security/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.project.spring.cleanmeet.common.security.config;
+
+
+import com.project.spring.cleanmeet.common.security.jwt.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((csrf)-> csrf.disable())
+                //세션 데이터를 생성을 막는 코드(jwt 토큰을 만들 예정)
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .addFilterBefore(jwtFilter, ExceptionTranslationFilter.class)
+
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/login/**","/users","/api/auth/login/**").permitAll()
+//                                .requestMatchers("/admin/**").hasAuthority("ADMIN")
+                                .anyRequest().authenticated()
+                )
+        ;
+        return http.build();
+    }
+}
