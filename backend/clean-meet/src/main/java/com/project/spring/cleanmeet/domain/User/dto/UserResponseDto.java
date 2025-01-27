73cfeb7dea29929ac780ca7a712e1d4f2a30bb38
KAH <kahback99@gmail.com>
2025-01-27 17:58:42 +0900

    [feat] 유저 응답 dto 생성

---
+++ backend/clean-meet/src/main/java/com/project/spring/cleanmeet/domain/User/dto/UserResponseDto.java
@@ -0,0 +1,18 @@
+package com.project.spring.cleanmeet.domain.User.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDto {
+    private String email;
+    private String password;
+    private String name;
+    private String contact;
+    private AddressRequestDto addressRequestDto;
+    private String role;
+}
