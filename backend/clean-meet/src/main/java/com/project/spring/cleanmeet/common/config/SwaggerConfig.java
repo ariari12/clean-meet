9514101cf8bff4def66ddc59103c8d654cbab80d
KAH <kahback99@gmail.com>
2025-02-02 03:31:56 +0900

    [docs] 스웨거 설정 추가

---
+++ backend/clean-meet/src/main/java/com/project/spring/cleanmeet/common/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.project.spring.cleanmeet.common.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "CleanMeet API", version = "1.0", description = "CleanMeet 프로젝트의 API 명세서"),
+        security = @SecurityRequirement(name = "bearerAuth")  // 보안 스키마 적용
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+}
