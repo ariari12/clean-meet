580070d8baff58cf35018ae58a1526ecf5b91498
KAH <kahback99@gmail.com>
2025-02-02 03:24:20 +0900

    [rename] 컨트롤러 이름 변경

---
+++ backend/clean-meet/src/main/java/com/project/spring/cleanmeet/common/security/jwt/controller/LoginApiController.java
@@ -0,0 +1,41 @@
+package com.project.spring.cleanmeet.common.security.jwt.controller;
+
+import com.project.spring.cleanmeet.common.security.jwt.service.AuthService;
+import com.project.spring.cleanmeet.common.security.jwt.dto.UserLoginRequestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class LoginApiController {
+    private final AuthService authService;
+
+
+    @Operation(summary = "JWT 로그인", description = "이메일과 비밀번호로 로그인하여 액세스 토큰과 리프레시 토큰을 발급받습니다.")
+    @PostMapping("/login")
+    public ResponseEntity<String> loginJwt(@RequestBody UserLoginRequestDto userLoginRequestDto,
+                                      HttpServletResponse httpServletResponse) {
+        String accessToken = authService.loginWithJwt(userLoginRequestDto, httpServletResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(accessToken);
+    }
+
+    @Operation(
+            summary = "리프레시 토큰을 사용하여 새로운 액세스 토큰 발급",
+            description = "쿠키에 저장된 리프레시 토큰을 사용해 만료된 액세스 토큰을 갱신합니다. ex) Bearer 토큰값",
+            security = @SecurityRequirement(name = "bearerAuth")
+    )
+    @PostMapping("/refreshToken")
+    public ResponseEntity<String> tokenRotation(@CookieValue("REFRESH_TOKEN") String refreshToken,
+                                                @RequestHeader("Authorization") String authorizationHeader,
+                                                HttpServletResponse response) {
+        String accessToken = authService.tokenRotation(refreshToken, authorizationHeader, response);
+
+        return ResponseEntity.status(HttpStatus.OK).body(accessToken);
+    }
+}
--- backend/clean-meet/src/main/java/com/project/spring/cleanmeet/domain/User/conrotller/UserApiController.java
+++ backend/clean-meet/src/main/java/com/project/spring/cleanmeet/domain/User/conrotller/UserApiController.java
@@ -2,6 +2,7 @@ package com.project.spring.cleanmeet.domain.User.conrotller;
 import com.project.spring.cleanmeet.domain.User.dto.UserRequestDto;
 import com.project.spring.cleanmeet.domain.User.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
-public class UserController {
+public class UserApiController {
     private final UserService userService;
 
 
+    @Operation(
+            summary = "회원가입",
+            description = "새로운 사용자를 등록합니다."
+    )
     @PostMapping
     public ResponseEntity<String> users(@RequestBody UserRequestDto userRequestDto) {
         Long userId = userService.save(userRequestDto);
