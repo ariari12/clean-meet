9c7aab5394d4956c0fb4feae7d6a3347c35659cd
kidohy15 <kidohy307@gmail.com>
2025-02-17 05:14:05 +0900

    style: 회사 목록 스타일 수정 & 회사 상세 페이지로 이동 추가

--- frontend/src/pages/company/index.tsx
+++ frontend/src/pages/company/index.tsx
@@ -4,21 +4,26 @@ import { FaSearch } from "react-icons/fa";
 
 const CompanyListPage = () => {
   const [search, setSearch] = useState("");
+  
   // 확인용 더미
-  const companies = new Array(17).fill(null);
+  const companies = [
+    {
+      id: 1,
+      name: "청소나라",
+      description: "새 집처럼 깨끗하게 청소해드립니다",
+    },
+    { id: 2, name: "클린마스터", description: "사무실 전문 청소 서비스" },
+    { id: 3, name: "청소의 달인", description: "특수 청소, 원룸 청소 전문" },
+    { id: 4, name: "청소왕", description: "당일 빠른 청소 서비스" },
+  ];
 
   return (
     <div>
       <div className="pt-[120px] bg-blue-500 text-white">
         <div className="max-w-7xl mx-auto py-10 px-6 flex justify-between items-center">
-          <div className="flex-1">
-            <h1 className="text-3xl font-semibold mb-4">
-              직접 의뢰를 등록하고 다양한 전문 업체들의 견적을 받아보세요!
-            </h1>
-            <p className="text-lg mb-6">
-              상황에 맞는 최적의 서비스를 선택할 수 있습니다.
-            </p>
-          </div>
+          <h1 className="text-3xl font-semibold mb-4">
+            직접 의뢰를 등록하고 다양한 전문 업체들의 견적을 받아보세요!
+          </h1>
           <Link href="/request/regist" passHref>
             <span className="max-w-[320px] block px-6 py-3 bg-zinc-950 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-white hover:text-zinc-950 transition cursor-pointer">
               의뢰 등록하기
@@ -44,20 +49,24 @@ const CompanyListPage = () => {
 
         {/* 리스트 영역 */}
         <div className="w-full grid grid-cols-4 gap-6">
-          {companies.map((_, index) => (
-            <div
-              key={index}
-              className="p-1 min-w-[150px] max-h-[370px] rounded-2xl bg-gray-50 shadow-md cursor-pointer border border-gray-300"
-            >
-              <div className="min-h-[150px] bg-gray-200 rounded-xl shadow-inner"></div>
-              <p className="text-lg font-semibold text-zinc-800 mt-4 ml-2">
-                새 집처럼 깨끗하게 청소해드립니다.
-              </p>
-              <p className="mt-3 ml-2 text-md text-zinc-500">청소나라</p>
-              <p className="mt-3 ml-2 text-xs text-gray-800">
-                #완벽한 청소 #사무실 전문 #특수 청소 #원룸 청소
-              </p>
-            </div>
+          {companies.map((company) => (
+            <Link key={company.id} href={`/company/${company.id}`} passHref>
+              <div className="flex flex-col justify-between p-1 min-w-[150px] h-full max-h-[370px] rounded-2xl bg-gray-50 shadow-md cursor-pointer border border-gray-300">
+                <div>
+                  <div className="min-h-[150px] bg-gray-200 rounded-xl shadow-inner"></div>
+                  <p className="mt-3 ml-2 text-md text-zinc-500">
+                    {company.name}
+                  </p>
+                  <p className="text-lg font-semibold text-zinc-800 mt-4 ml-2">
+                    {company.description}
+                  </p>
+                </div>
+
+                <p className="mt-5 mx-2 py-2 text-sm text-gray-800">
+                  #완벽한 청소 #사무실 전문 #특수 청소 #원룸 청소
+                </p>
+              </div>
+            </Link>
           ))}
         </div>
 
