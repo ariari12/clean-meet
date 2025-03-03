e85fb5112a2f11c96084b620dc12dbdc813cfbef
kidohy15 <kidohy307@gmail.com>
2025-03-04 08:50:59 +0900

    fix: 회사회원가입 데이터 폼 수정

--- frontend/src/components/CompanySignup.tsx
+++ frontend/src/components/CompanySignup.tsx
@@ -13,6 +13,17 @@ const CompanySignup = () => {
   const [contact, setContact] = useState<string>("");
   const [addressName, setAddressName] = useState<string>("");
   const [companyName, setCompanyName] = useState<string>("");
+  const [serviceArea, setServiceArea] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [companyContact, setCompanyContact] = useState<string>("");
+  const [serviceCategory, setServiceCategory] = useState<string>("");
+  // const [region1DepthName, setRegion1DepthName] = useState<string>("");
+  // const [region2DepthName, setRegion2DepthName] = useState<string>("");
+  // const [region3DepthName, setRegion3DepthName] = useState<string>("");
+  // const [roadName, setRoadName] = useState<string>("");
+  // const [mainBuildingNo, setMainBuildingNo] = useState<string>("");
+  // const [subBuildingNo, setSubBuildingNo] = useState<string>("");
+  // const [zoneNo, setZoneNo] = useState<string>("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -23,25 +34,32 @@ const CompanySignup = () => {
     }
 
     const submitData = {
+      businessNumber: "", // 사업자번호 입력 추가
       companyName,
-      email,
-      password,
-      name,
-      contact,
-      addressRequestDto: {
-        addressName,
-        region1DepthName: "",
-        region2DepthName: "",
-        region3DepthName: "",
-        roadName: "",
-        mainBuildingNo: "",
-        subBuildingNo: "",
-        zoneNo: "",
+      serviceArea,
+      description,
+      companyContact,
+      serviceCategory: [{ name: serviceCategory }],
+      userRequestDto: {
+        email,
+        password,
+        name,
+        contact,
+        addressRequestDto: {
+          addressName,
+          region1DepthName: "",
+          region2DepthName: "",
+          region3DepthName: "",
+          roadName: "",
+          mainBuildingNo: "",
+          subBuildingNo: "",
+          zoneNo: "",
+        },
       },
     };
 
     try {
-      const apiUrl = `${API_BASE_URL}/api/users/company`; // 기업 회원가입
+      const apiUrl = `${API_BASE_URL}/api/companies`; // 기업 회원가입 API URL
 
       await axios.post(apiUrl, submitData);
       alert("회원가입 성공");
@@ -53,6 +71,7 @@ const CompanySignup = () => {
 
   return (
     <form className="mt-[20px]" onSubmit={handleSubmit}>
+      {/* 담당자 이름 */}
       <input
         type="text"
         id="name"
@@ -61,6 +80,8 @@ const CompanySignup = () => {
         placeholder="담당자 이름"
         required
       />
+
+      {/* 이메일 */}
       <input
         type="email"
         id="email"
@@ -69,6 +90,8 @@ const CompanySignup = () => {
         placeholder="이메일"
         required
       />
+
+      {/* 비밀번호 */}
       <input
         type="password"
         id="password"
@@ -77,6 +100,8 @@ const CompanySignup = () => {
         placeholder="비밀번호"
         required
       />
+
+      {/* 비밀번호 확인 */}
       <input
         type="password"
         id="confirmPassword"
@@ -85,6 +110,8 @@ const CompanySignup = () => {
         placeholder="비밀번호 확인"
         required
       />
+
+      {/* 연락처 */}
       <input
         type="text"
         id="contact"
@@ -93,6 +120,8 @@ const CompanySignup = () => {
         placeholder="연락처"
         required
       />
+
+      {/* 회사명 */}
       <input
         type="text"
         id="companyName"
@@ -101,6 +130,47 @@ const CompanySignup = () => {
         placeholder="회사명"
         required
       />
+
+      {/* 서비스 지역 */}
+      <input
+        type="text"
+        id="serviceArea"
+        className="w-full bg-white border-none p-[15px] px-[20px] rounded-[20px] mt-[15px] shadow-[#cff0ff_0px_5px_10px_-5px] focus:outline-none focus:border-2 focus:border-[#12b1d1] placeholder:text-[#aaaaaa]"
+        onChange={(e) => setServiceArea(e.target.value)}
+        placeholder="서비스 지역"
+        required
+      />
+
+      {/* 설명 */}
+      <textarea
+        id="description"
+        className="w-full bg-white border-none p-[15px] px-[20px] rounded-[20px] mt-[15px] shadow-[#cff0ff_0px_5px_10px_-5px] focus:outline-none focus:border-2 focus:border-[#12b1d1] placeholder:text-[#aaaaaa]"
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="설명"
+        required
+      />
+
+      {/* 회사 연락처 */}
+      <input
+        type="text"
+        id="companyContact"
+        className="w-full bg-white border-none p-[15px] px-[20px] rounded-[20px] mt-[15px] shadow-[#cff0ff_0px_5px_10px_-5px] focus:outline-none focus:border-2 focus:border-[#12b1d1] placeholder:text-[#aaaaaa]"
+        onChange={(e) => setCompanyContact(e.target.value)}
+        placeholder="회사 연락처"
+        required
+      />
+
+      {/* 서비스 카테고리 */}
+      <input
+        type="text"
+        id="serviceCategory"
+        className="w-full bg-white border-none p-[15px] px-[20px] rounded-[20px] mt-[15px] shadow-[#cff0ff_0px_5px_10px_-5px] focus:outline-none focus:border-2 focus:border-[#12b1d1] placeholder:text-[#aaaaaa]"
+        onChange={(e) => setServiceCategory(e.target.value)}
+        placeholder="서비스 카테고리"
+        required
+      />
+
+      {/* 주소 */}
       <input
         type="text"
         id="addressName"
@@ -109,6 +179,7 @@ const CompanySignup = () => {
         placeholder="주소"
         required
       />
+
       <input
         type="submit"
         className="block w-full font-bold bg-gradient-to-r from-[#132b39] to-[#085260] text-white py-[15px] mt-[20px] mx-auto rounded-[20px] shadow-[rgba(133,189,215,0.88)_0px_20px_10px_-15px] border-none transition-transform duration-200 ease-in-out hover:scale-[1.03] hover:shadow-[rgba(133,189,215,0.88)_0px_23px_10px_-20px] active:scale-[0.95] active:shadow-[rgba(133,189,215,0.88)_0px_15px_10px_-10px]"
